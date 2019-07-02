0e8ec226c948223efd4f4fae5e041d07c45dafdc
Janet Mendez <janetmendez.03@gmail.com>
2019-07-02 06:13:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,5 @@
 def reverse_each_word(s)
   reverse = []
-  s.split(" ").map{|e| do
-    reverse_word = []
-    e.split("").map{|w| reverse_word.unshift(reverse_word)
-    reverse_word.join("")
-    }
-    end}
+  s.split(" ").map{|e| reverse.push(e.split("").reverse().join(""))}
   reverse.join("")
 end
