3b79a3b254671301c99ace2ead730d4982039c08
Janet Mendez <janetmendez.03@gmail.com>
2019-07-02 06:12:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,10 @@
 def reverse_each_word(s)
   reverse = []
-  s.split(" ").map{|e| reverse.unshift(e)}
+  s.split(" ").map{|e| do
+    reverse_word = []
+    e.split("").map{|w| reverse_word.unshift(reverse_word)
+    reverse_word.join("")
+    }
+    end}
   reverse.join("")
 end
